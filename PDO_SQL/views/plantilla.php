922dc8f3cc70e86545db8ca1661202bc843da5cc
Juan <satelimportadores@gmail.com>
2020-10-15 08:50:12 -0500

    Redireccion a pagina 404

--- PDO_SQL/views/plantilla.php
+++ PDO_SQL/views/plantilla.php
@@ -26,26 +26,74 @@
 			<h3 class="text-center py-3">LOGO</h3>
 		</div>
 
-<!-- Navegacion -->		
-
+<!-- Navegacion -->	
+	
 		<div class="container-fluid">
 			<div class="container">
 				<ul class="nav nav-justified py-2 nav-pills">
-					<li class="nav-item">
-						<a class="nav-link " href="index.php?pagina=registro">Registro</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="index.php?pagina=salir">Salir</a>
-					</li>
+					<?php if (isset($_GET['pagina'])): ?>
+
+						<?php if ($_GET['pagina'] == "registro"): ?>
+							<li class="nav-item">
+								<a class="nav-link active" href="index.php?pagina=registro">Registro</a>
+							</li>
+							<?php else: ?>
+								<li class="nav-item">
+									<a class="nav-link" href="index.php?pagina=registro">Registro</a>
+								</li>
+							<?php endif ?>
+
+							<?php if ($_GET['pagina'] == "ingreso"): ?>
+								<li class="nav-item">
+									<a class="nav-link active" href="index.php?pagina=ingreso">Ingreso</a>
+								</li>
+								<?php else: ?>
+									<li class="nav-item">
+										<a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
+									</li>
+								<?php endif ?>
+
+								<?php if ($_GET['pagina'] == "inicio"): ?>
+									<li class="nav-item">
+										<a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
+									</li>
+									<?php else: ?>
+										<li class="nav-item">
+											<a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
+										</li>
+									<?php endif ?>
+
+							<?php if ($_GET['pagina'] == "salir"): ?>
+									<li class="nav-item">
+										<a class="nav-link active" href="index.php?pagina=salir">Salir</a>
+									</li>
+									<?php else: ?>
+										<li class="nav-item">
+											<a class="nav-link" href="index.php?pagina=salir">Salir</a>
+										</li>
+									<?php endif ?>
+
+									<?php else: ?>
+										<li class="nav-item">
+											<a class="nav-link active" href="index.php?pagina=registro">Registro</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
+										</li>
+										<li class="nav-item">
+											<a class="nav-link" href="index.php?pagina=salir">Salir</a>
+										</li>
+
+
+						<?php endif ?>
 				</ul>
 			</div>
 		</div>
+
+<!-- Navegacion -->
 
 <!-- contenido -->
 
