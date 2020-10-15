102ab57baa4f5b765a44642d2ca26778f97bbef2
Juan <satelimportadores@gmail.com>
2020-10-15 15:09:26 -0500

    Crear datos en la BD

--- PDO_SQL/index.php
+++ PDO_SQL/index.php
@@ -1,8 +1,13 @@
 <?php
 	require_once "controllers/plantillaControlador.php";
 	require_once "controllers/formularioControlador.php";
+	require_once "models/conexion.php";
 	#mostraremos las vistas a los usuarios:
 	$plantilla = new ControladorPlantilla();
-	$plantilla -> ctrTraerPlantilla();	
+	$plantilla -> ctrTraerPlantilla();
+
+	$conexion = Conexion::conectar();
+
+	echo print_r($conexion);
 
 ?>
